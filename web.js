fb92b4a7c51ba7255ce0ce09fd62eb107cdb48e8
Ryan Daigle <ryan.daigle@gmail.com>
2011-12-22 15:59:42 -0500

    Add in explicit use of models

--- web.js
+++ web.js
@@ -2,7 +2,8 @@ var express = require('express'),
   resource = require('express-resource'),
   app = express.createServer(express.logger());
 
-app.resource('units', require('./controllers/unit'));
+var Unit = require('./models/unit')
+app.resource('units', require('./controllers/units_controller'), { load: Unit.find });
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("PTLS web process started");
