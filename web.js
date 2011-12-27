a0e0c83d35770663f96ed147d128de9adc1b325a
Ryan Daigle <ryan.daigle@gmail.com>
2011-12-27 15:25:39 -0500

    Playing w/ app config

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 var express = require('express'),
-  resource = require('express-resource'),
-  app = express.createServer(express.logger());
+	resource = require('express-resource');
+var	app = express.createServer(
+	express.logger({ format: ':method :url :status in :response-time ms' }),
+	express.bodyParser()
+);
 
 var Unit = require('./models/unit')
 app.resource('units', require('./controllers/units_controller'), { load: Unit.find });
