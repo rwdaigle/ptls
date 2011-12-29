fd9a157b30746fb94fc76968294b8e4e03fc1b02
Ryan Daigle <ryan.daigle@gmail.com>
2011-12-29 14:16:24 -0500

    Rename helpers => filters

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
 
-	var models = require('../helpers/models');
+	var models = require('../filters/models');
 
 	// Units
 	var unitsController = require('../controllers/units'),
