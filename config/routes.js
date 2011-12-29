bc87a0175fea10e2147c9e656ddd7916334e65b1
Ryan Daigle <ryan.daigle@gmail.com>
2011-12-29 13:33:29 -0500

    Move model resource helpers to their own location

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,19 @@
 module.exports = function(app) {
 
+	app.param('unitId', function(req, res, next, id) {
+		Unit.load(id, function(err, unit) {
+			if (err) return next(err);
+			if (!unit) return next(new Error('Failed to find unit with id: ' + id));
+			req.model = unit;
+			next();
+		});
+	});
+
 	var models = require('../helpers/models');
 
 	// Units
 	var unitsController = require('../controllers/units'),
 		unit = require('../models/unit');
-	app.get('/units/:id.:format?', models.setModel('id', unit.load), unitsController.show);
+	app.get('/units/:unitId.:format?', unitsController.show);
+	// , models.setModel('unitId', unit.load)
 }
